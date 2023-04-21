eb2d0bdb1a70e2c796747f06e9ce8f3f168aa409
yun <yun@yka.moe>
2023-04-21 15:48:04 +0800

    更新建立内容

--- config/config.js
+++ config/config.js
@@ -4,75 +4,80 @@ import {faAppStore, faDocker, faGithub, faGooglePlay} from '@fortawesome/free-br
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-	name: "yka",
+	name: "yun",
 	links: [
 		{
-			title: "About",
+			title: "关于",
 			link: "#about",
 		},
 		{
-			title: "Projects",
+			title: "项目",
 			link: "#projects",
 		},
 		{
-			title: "Contact",
+			title: "联系",
 			link: "#contact",
 		},
 		{
-			title: "Links",
+			title: "链接",
 			link: "/links",
 		},
 	],
 }
 export const intro = {
-	title: "Hey, I'm yka",
-	description: "Learn about the world through technology.",
+	title: "你好，我是yun",
+	description: "通过技术了解世界",
 	image: profile.src,
 	buttons: [
 		{
-			title: "Contact Me",
+			title: "联系我",
 			link: "#contact",
 			isPrimary: true,
 		},
-		{
-			title: "Resume",
-			link: "#",
-			isPrimary: false,
-		},
+		// {
+		// 	title: "简历",
+		// 	link: "#",
+		// 	isPrimary: false,
+		// },
 	],
 }
 
 export const about = {
-	title: "Who I am",
+	title: "我是谁",
 	description: [
-		"I graduated from Chongqing Three Gorges University National University of Science and Technology (CTGU) in 2018 with a degree in Computer Engineering. I started my career as a Java backend developer working for Jiuyuan Yinhai, a traditional enterprise located in China.",
-		"The tech industry is constantly evolving, and I love being able to grow with it while building on the foundations. I open source my code from an empathy standpoint - for future developers, teammates, users, and with accessibility in mind. I also enjoy being part of the community, helping and mentoring new developers, and supporting organizations that promote diversity in technology.",
-		"When I'm not programming, I focus on my hobbies: blogging, playing games, meeting people, and growing my network."
+		"我于 2018 年毕业于重庆三峡学院计算机工程专业。 我的职业生涯始于中国传统企业久远银海的 Java 后端开发人员。",
+		"科技行业在不断发展，我喜欢在打好基础的同时与它一起成长。 我从同理心的角度开源我的代码——为了未来的开发人员、队友、用户，并且考虑到可访问性。 我也喜欢成为社区的一员，帮助和指导新的开发人员，并支持促进技术多样性的组织。",
+		"当我休息的时候，我会专注于我的爱好：写博客、玩游戏、结识新朋友以及拓展我的站群。"
 	],
 }
 
 export const work = {
-	title: "What I do",
+	title: "我做什么",
 	cards: [
 		{
-			title: "Java Development",
-			description: "I code the backend in Java.",
+			title: "Java开发(鸽了很久)",
+			description: "作为后端开发",
 			icons: null,
 		},
 		{
-			title: "Kotlin Development",
-			description: "I create any funny program using Kotlin.",
+			title: "Kotlin开发(也快鸽了)",
+			description: "作为后端开发",
 			icons: null,
-		}
+		},
+		{
+			title: "nodejs开发",
+			description: "作为后端开发",
+			icons: null,
+		},
 	],
 }
 
 export const projects = {
-	title: "Projects",
+	title: "项目",
 	cards: [
 		{
 			title: "UrlRandom",
-			description: "A web program that records urls for random access when needed.",
+			description: "记录url以供随机链接访问",
 			icons: [
 				{
 					icon: faDocker,
@@ -84,53 +89,59 @@ export const projects = {
 				},
 			]
 		},
+		{
+			title: "UrlRandom",
+			description: "一个kotlin的时间轴数据生成工具",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/yunkuangao/timeline-data",
+				},
+			]
+		},
 	],
 }
 
 export const contact = {
-	title: "Get in touch",
-	description: "Tea party! Please don't hesitate to post at https://flarum.yunkuangao.com. Alternatively, feel free to contact yun@yunkuangao.com directly via email.",
+	title: "保持联系",
+	description: "闲聊！ 请不要犹豫，在 https://ele.yka.moe(matrix客户端) 上与 @yun:im.yka.moe 联系。 或者，随时通过电子邮件直接联系 yun@yka.moe",
 	buttons: [
 		{
-			title: "Email Me",
-			link: "mailto:yun@yunkuangao.com",
+			title: "发送邮件",
+			link: "mailto:yun@yka.moe",
 			isPrimary: true,
 		},
 		{
-			title: "Post Topic",
-			link: "https://forum.yunkuangao.com",
+			title: "云聊天",
+			link: "https://ele.yka.moe",
 			isPrimary: false,
 		},
 	]
 }
 
-// SEARCH ENGINE 
+// SEARCH ENGINE
 export const SEO = {
-	// 50 - 60 char  
-	title: "yka | Computer Engineer | Java | Kotlin developer",
-	description: "I create websites and applications. I graduated from Chongqing Three Gorges University (NUSTCTGU) in 2018 with a degree in Computer Engineering.",
+	// 50 - 60 char
+	title: "yun | 软件工程师 | 自由主义者",
+	description: "创建并维护云站群，并与世界时刻保持联系。",
 	image: profile.src,
 }
 
 export const links = {
 	image: profile.src,
-	title: "@yka",
+	title: "@yun",
 	description: "Computer Engineer | Java | Kotlin Developer",
 	cards: [
 		{
-			title: "My website",
-			link: "https://yunkuangao.me/",
+			title: "云站群",
+			link: "https://home.yka.moe/",
 		},
 		{
-			title: "Image Hosting",
-			link: "https://yunkuangao.com/",
-		},
-		{
-			title: "Docker home",
+			title: "Docker",
 			link: "https://hub.docker.com/u/yunkuangao/",
 		},
 		{
-			title: "My GitHub",
+			title: "Github",
 			link: "https://github.com/yunkuangao/",
 		},
 	]
